240d235f388da4367b2e4587ee3174b3fbd2b415
yash161 <yashshah19@gnu.ac.in>
2022-10-11 02:13:15 +0530

    new73

--- database.js
+++ database.js
@@ -7,6 +7,6 @@ var con =mysql.createPool(
             // database:"project"
             host: "terraform-20221010200318227600000001.cwq1rj9hzdnj.us-east-1.rds.amazonaws.com",
             user: "yash",
-            password: "yash@123"
+            password: "yashshah"
         });
 module.exports = con;
