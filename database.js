c74ae761874c459854e3af8e3994cfbed1749e1f
yash161 <yashshah19@gnu.ac.in>
2022-10-11 02:17:28 +0530

    new75

--- database.js
+++ database.js
@@ -1,12 +1,8 @@
 var mysql=require('mysql');
 var con =mysql.createPool(
     {
-            // host:"localhost",
-            // user:"root",
-            // password:"yash@123",
-            // database:"project"
-            host: "terraform-20221010200318227600000001.cwq1rj9hzdnj.us-east-1.rds.amazonaws.com",
-            user: "yash",
-            password: "yashshah"
+        host: "terraform-20221010200318227600000001.cwq1rj9hzdnj.us-east-1.rds.amazonaws.com",
+        user: "yash",
+        password: "yashshah"
         });
 module.exports = con;
