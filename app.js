aa5141daaac1ed41471c7df006e4fbf910541125
yash161 <yashshah19@gnu.ac.in>
2022-11-10 02:23:57 +0530

    com3

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
+const checksum_lib = require("./Paytm/checksum");
+const config = require("./Paytm/config");
 var mysql = require('mysql');
 const jsonToTable = require('json-to-table');
 const path2 = __dirname + '/views/';
 const app = express();
 const json2html = require('node-json2html');
 const port = 8000;
+const parseUrl = express.urlencoded({ extended: false });
+const parseJson = express.json({ extended: false });
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 var mysql = require('mysql');
@@ -307,6 +311,43 @@ app.get('/pay', (req, res) => {
 app.get('/top', (req, res) => {
     res.sendFile(path2 + 'table.html')
 })
+app.post("/paynow", [parseUrl, parseJson], (req, res) => {
+    var paymentDetails = {
+      amount: req.body.amount,
+      customerId: req.body.name,
+      customerEmail: req.body.email,
+      customerPhone: req.body.phone
+  }
+
+      var params = {};
+      params['MID'] = config.PaytmConfig.mid;
+      params['WEBSITE'] = config.PaytmConfig.website;
+      params['CHANNEL_ID'] = 'WEB';
+      params['INDUSTRY_TYPE_ID'] = 'Retail';
+      params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
+      params['CUST_ID'] = paymentDetails.customerId;
+      params['TXN_AMOUNT'] = paymentDetails.amount;
+    //   params['CALLBACK_URL'] = 'http://localhost:3000/callback';
+      params['EMAIL'] = paymentDetails.customerEmail;
+      params['MOBILE_NO'] = paymentDetails.customerPhone;
+  
+  
+      checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+          var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+          // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+  
+          var form_fields = "";
+          for (var x in params) {
+              form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
+          }
+          form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
+  
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+          res.end();
+      });
+  
+  });
 app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
 });
