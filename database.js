dbd5a2b4ba61ab340690559e48d330d4bd4ba762
yash161 <yashshah19@gnu.ac.in>
2022-11-10 12:13:16 +0530

    terds62

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ var mysql=require('mysql');
 //         port:3306
 
 //         });
-const con = mysql.createConnection({
+const con = mysql.createPool({
     host: "terraform-20221110045414759600000001.cwq1rj9hzdnj.us-east-1.rds.amazonaws.com",
     user: "yash",
     password: "yashshah",
