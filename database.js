2ca8edc2baea22fc5216ea268101b345d112058e
yash161 <yashshah19@gnu.ac.in>
2022-11-10 11:53:23 +0530

    terds

--- database.js
+++ database.js
@@ -1,12 +1,17 @@
 
 var mysql=require('mysql');
-var con =mysql.createPool(
-    {
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database:"project",
-        port:3306
+// var con =mysql.createPool(
+//     {
+//         host: "localhost",
+//         user: "root",
+//         password: "root",
+//         database:"project",
+//         port:3306
 
-        });
+//         });
+const con = mysql.createConnection({
+    host: "terraform-20221110045414759600000001.cwq1rj9hzdnj.us-east-1.rds.amazonaws.com>",
+    user: "yash",
+    password: "yashshah"
+});
 module.exports = con;
